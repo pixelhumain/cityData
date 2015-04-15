66644a01941ed10509c5929fc784dc1310609be6
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2015-04-15 15:07:43 +0200

    cityData server in Scala: SPARQL generation - WIP TODO : run the SPARQL query

--- cityData_server_scala/app/models/REST_SPARQL_bridge.scala
+++ cityData_server_scala/app/models/REST_SPARQL_bridge.scala
@@ -8,12 +8,26 @@ object REST_SPARQL_bridge {
   val dbo = "http://dbpedia.org/ontology/"
   val dbp = "http://dbpedia.org/resource/"
 
-  
-  /** For URI's like
+  val queryBeginning = """
+    PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+    PREFIX dbpedia:     <http://dbpedia.org/resource/>
+    CONSTRUCT {
+      ?CITY ?P ?O .
+    }
+    WHERE {
+      ?CITY a dbpedia-owl:Settlement ;
+    """
+  val queryEnding = """
+              ?P ?O .
+    }
+    """
+
+  /**
+   * For URI's like
    *   /baseURL/cities/fr/01600
-   * 
+   *
    * create SPARQL query: <pre>
-   * 
+   *
    * PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
    * PREFIX dbpedia:     <http://dbpedia.org/resource/>
    * CONSTRUCT {
@@ -26,23 +40,42 @@ object REST_SPARQL_bridge {
    *         ?P ?O .
    * }
    * <pre>
-   * 
-   *   */
+   *
+   */
   def getJSONLD(request: Request[AnyContent]): String = {
     println("getJSONLD: " + request)
+    val conf = RequestRDFConfig(Seq(
+      new RequestRDFPathName(dbo + "country") {
+        val iso3166ToDBPedia = Map(
+          "fra" -> (dbp + "France"))
+        override def map(value: String): String = wrapAsURI(iso3166ToDBPedia.get(value).get)
+      },
+      RequestRDFPathName(dbo + "postalCode")))
+    val queryString = makeSPARQL(request, conf)
+    makeQuery(queryString)
+  }
+
+  private def makeSPARQL(request: Request[AnyContent], conf: RequestRDFConfig): String = {
     val path = request.path
-    val elems =  path.split("/")
-    
-    val conf = RequestRDFConfig( Seq(
-        RequestRDFPathName(dbo + "country"),
-        RequestRDFPathName(dbo + "postalCode")
-        ))
-    // TODO >>>>>>>>>>>>>>
-    "bla"
+    val elems = path.split("/")
+    // elems contains e.g.  "fr", "01600"
+    val propsAndValues = conf.mappings.zip(elems)
+    val criteria: Seq[String] = propsAndValues.map {
+      x =>
+        wrapAsURI(x._1.uri) + " " + x._1.map(x._2) + " ;\n"
+    }
+    queryBeginning + criteria.mkString + queryEnding
   }
-  case class RequestRDFPathName(
-      // name: String, 
-      uri: String)
-  case class RequestRDFConfig( mappings: Seq[RequestRDFPathName]) {  
+
+  def wrapAsURI(uri: String) = s"<$uri>"
+
+  case class RequestRDFPathName(uri: String) {
+    def map(value: String): String = value
   }
+  case class RequestRDFConfig(mappings: Seq[RequestRDFPathName])
+
+  def makeQuery(queryString: String): String = {
+    ???
+  }
+
 }
